fe1bfa104cf0b08f5670cb50ca6c7ac16f4c32eb
youme <youme.bae@gmail.com>
2019-02-13 17:16:02 +0900

    Update Team.java

--- src/main/java/iterator/heroes/case2/Team.java
+++ src/main/java/iterator/heroes/case2/Team.java
@@ -16,7 +16,19 @@ public class Team extends AbstractCollection<Hero>{
 	public Iterator<Hero> iterator() {
 		// TODO 02. anonymous inner type의 java.util.Iterator 를 생성하여 return한다.
 		// TODO 03. java.util.Iterator 에서 구현해야할 메소드(hasNext(),next())를 생성하여 구현한다.
-		return null;
+		return new Iterator<Hero>() {
+
+			int cursor;
+			@Override
+			public boolean hasNext() {
+				return cursor != last;
+			}
+
+			@Override
+			public Hero next() {
+				return heroes[cursor++];
+			}
+		};
 	}
 
 	@Override
@@ -27,6 +39,7 @@ public class Team extends AbstractCollection<Hero>{
 	// TODO 04. AbstractCollection 에서 오라이드된 add 메소드를 구현한다.
 	@Override
 	public boolean add(Hero hero) {
-		return false;
+		heroes[last++] = hero;
+		return true;
 	}
 }
