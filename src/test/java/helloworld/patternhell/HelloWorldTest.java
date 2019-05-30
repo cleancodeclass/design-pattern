0ea9c6196cb918ba16a46c987dd82aa547e18685
doni <ldspisee@gmail.com>
2019-05-30 20:49:54 +0900

    add contents

---
+++ src/test/java/helloworld/patternhell/HelloWorldTest.java
@@ -0,0 +1,37 @@
+package helloworld.patternhell;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloWorldTest {
+	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
+	private final PrintStream originalErr = System.err;
+
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	    System.setErr(new PrintStream(errContent));
+	}
+
+	@After
+	public void restoreStreams() {
+	    System.setOut(originalOut);
+	    System.setErr(originalErr);
+	}
+	
+	@Test
+	public void test() {
+		HelloWorld.main(new String[] {});
+		assertEquals("Hello World!\r\n", outContent.toString());
+	}
+
+}
